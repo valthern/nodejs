d40ecdb4c3d7bbcf7d25d4d59a4b69a0d8bf6305
Omar I. Martínez Gómez <omar_238@hotmail.com>
2021-04-20 17:09:00 -0500

    casí terminamos la seccion 6 del curso

--- 0207 inicializacion npm/app.js
+++ 0207 inicializacion npm/app.js
@@ -4,9 +4,22 @@ const pug = require("pug")
 
 app.use(express.static(__dirname + "/public"))
 
+let perros_array = [
+	{raza:"Doberman", texto:"Perro de ataque", imagen:"doberman.jpg"},
+	{raza:"Dachshund", texto:"Perro de caza", imagen:"dachshund.jpg"},
+	{raza:"Pastor Alemán", texto:"Perro de pastoreo", imagen:"pastorAleman.jpg"},
+	{raza:"Pug", texto:"Peroo de compañía", imagen:"pug.jpg"},
+	{raza:"San Bernardo", texto:"Perro de rescate", imagen:"sanBernardo.jpg"}
+]
+
 app.get("/", (req, res) => {
 	//res.send("index.html")
-	res.render("index.pug")
+	res.render("index.pug", {
+		titulo: "Perros del mundo",
+		texto: "Selecciona un perro",
+		imagen: "perros.jpg",
+		perros: perros_array
+	})
 })
 
 app.listen(3000, () => {
