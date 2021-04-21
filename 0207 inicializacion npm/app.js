15eee6ba37bd0e19fc958ff94b1118ae53beb85a
Omar I. Martínez Gómez <omar_238@hotmail.com>
2021-04-20 23:53:15 -0500

    amores perros

--- 0207 inicializacion npm/app.js
+++ 0207 inicializacion npm/app.js
@@ -8,18 +8,36 @@ let perros_array = [
 	{raza:"Doberman", texto:"Perro de ataque", imagen:"doberman.jpg"},
 	{raza:"Dachshund", texto:"Perro de caza", imagen:"dachshund.jpg"},
 	{raza:"Pastor Alemán", texto:"Perro de pastoreo", imagen:"pastorAleman.jpg"},
-	{raza:"Pug", texto:"Peroo de compañía", imagen:"pug.jpg"},
+	{raza:"Pug", texto:"Perro de compañía", imagen:"pug.jpg"},
 	{raza:"San Bernardo", texto:"Perro de rescate", imagen:"sanBernardo.jpg"}
 ]
 
 app.get("/", (req, res) => {
-	//res.send("index.html")
 	res.render("index.pug", {
 		titulo: "Perros del mundo",
 		texto: "Selecciona un perro",
 		imagen: "perros.jpg",
 		perros: perros_array
 	})
+})
+
+app.get("/perro/:raza", (req, res) => {
+	let datosPerro = perros_array.filter((perro) => {
+		if (req.params.raza == perro.raza) {
+			return perro
+		}
+	})[0]
+
+	res.render("perro.pug", {
+		raza: req.params.raza,
+		data: datosPerro
+	})
+})
+
+app.use((req, res) => {
+	res.status(400)
+	let error = req.originalUrl
+	res.render("404.pug", { texto: error })
 })
 
 app.listen(3000, () => {
